3f1022fb69447ae8df914152b3a9ed9c6f035277
Giovanny Garzón Soto <giovannygarzonsoto@gmail.com>
2024-06-01 01:36:51 -0400

    footer details

--- src/layout/Footer.vue
+++ src/layout/Footer.vue
@@ -1,6 +1,32 @@
 <template>
     <div class="footer">
-        <img @click="toMain" class="footer__logo" src="../../public/logo.png" alt="logo">
+        <div class="footer__logo">
+            <img @click="toMain" class="footer__logo" src="../../public/logo.png" alt="logo">
+            <span>©︎2019 Shueisha Inc.</span>
+            <span>Todos los derechos reservados.</span>
+        </div>
+        <div class="footer__info">
+            <div class="footer__links">
+                <a href="">Asistencia</a>
+                <a href="">Noticias y <br/>Eventos</a>
+                <a href="">Política de <br/>Privacidad</a>
+                <a href="">Términos <br/>de Uso</a>
+                <a href="">Clasificaciones <br/>de Contenido</a>
+                <a href="">Derechos <br/>de Autor</a>
+            </div>
+            <div class="footer__desc">
+                <img src="https://mangaplus.shueisha.co.jp/img/abj1092_1042.6c597180.png" alt="logo2">
+                <p>ABJ Mark is a registered trademark (Registration No.10921042)<br/>
+                    indicating that this e-book store / e-book distribution service is an authorized distribution
+                    service that gained permission to use content from the copyright holder.<br/>
+                    For more information check https://aebs.or.jp/.</p>
+            </div>
+        </div>
+        <div class="footer__apps">
+            <!-- <img @click="toMain" class="footer__logo" src="../../public/logo.png" alt="logo">
+            <span>©︎2019 Shueisha Inc.</span>
+            <span>Todos los derechos reservados.</span> -->
+        </div>
     </div>
 </template>
 
@@ -14,7 +40,7 @@ export default defineComponent({
     setup() {
         const router: Router = useRouter()
 
-        const { toMain } =  useTo(router)
+        const { toMain } = useTo(router)
 
         return {
             toMain
